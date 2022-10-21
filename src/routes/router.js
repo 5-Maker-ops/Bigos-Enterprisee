e2ebfaf0be4d66d58c70d5fcd063884d16868879
5-Marker-ops <momodoujah5@gmail.com>
2022-10-21 14:34:43 +0000

    javacript

---
+++ src/routes/router.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const { createUser } = require('../controllers/user');
+
+
+// User route
+router.post('/user', createUser);
+
+
+module.exports = router;
+const express = require('express');
+const app = express();
+
+const connect = require('./modules/connect');
+const routes = require('./routes/router');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Connect to Database
+void (async () => {
+ try {
+  await connect();
+  console.log('connected to database');
+ } catch (error) {
+  console.log('error connecting to database:', error.message);
+ }
+})();
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Peace' });
+   });
+   
+   
+   app.use('/api', routes);
+   
+   
+   module.exports = app;
+ 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
  const app = express();
  
+ const connect = require('./modules/connect');
  
  
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
+
+ 
  
  
  app.get('/', (req, res) => {
   res.status(200).json({ message: 'Peace' });
  });
+   // Connect to Database
+   void (async () => {
+    try {
+     await connect();
+     console.log('connected to database');
+    } catch (error) {
+     console.log('error connecting to database:', error.message);
+    }
+   })();
+
+   
  
  
  module.exports = app;
